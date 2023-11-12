263b626aaa96475b247f07825ad317c4c225a9ba
vspaz <vladimir.edelman@gmail.com>
2023-11-12 12:43:20 +0100

    change default deserilization behavior

--- src/userdata.rs
+++ src/userdata.rs
@@ -1,57 +1,61 @@
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize, Default)]
+
 struct Info {
+    #[serde(skip_deserializing)]
     pub seed: String,
+    #[serde(skip_deserializing)]
     pub results: i64,
+    #[serde(skip_deserializing)]
     pub page: i64,
+    #[serde(skip_deserializing)]
     pub version: String,
 }
 
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 struct Id {
-    pub name: String,
-    pub value: String,
+    pub name: Option<String>,
+    pub value: Option<String>,
 }
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 struct DateOfBirth {
-    pub date: String,
-    pub age: i64,
+    pub date: Option<String>,
+    pub age: Option<i64>,
 }
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 struct Login {
-    pub uuid: String,
-    pub username: String,
-    pub password: String,
-    pub salt: String,
-    pub md5: String,
-    pub sha1: String,
-    pub sha256: String,
+    pub uuid: Option<String>,
+    pub username: Option<String>,
+    pub password: Option<String>,
+    pub salt: Option<String>,
+    pub md5: Option<String>,
+    pub sha1: Option<String>,
+    pub sha256: Option<String>,
 }
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 struct Timezone {
-    pub offset: String,
-    pub description: String,
+    pub offset: Option<String>,
+    pub description: Option<String>,
 }
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 struct Coordinates {
-    pub latitude: String,
-    pub longitude: String,
+    pub latitude: Option<String>,
+    pub longitude: Option<String>,
 }
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 struct Street {
-    pub number: i64,
-    pub name: String,
+    pub number: Option<i64>,
+    pub name: Option<String>,
 }
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 struct Location {
-    pub street: Street,
-    pub city: String,
-    pub state: String,
-    pub country: String,
-    pub postcode: String,
-    pub coordinates: Coordinates,
-    pub timezone: Timezone,
+    pub street: Option<Street>,
+    pub city: Option<String>,
+    pub state: Option<String>,
+    pub country: Option<String>,
+    pub postcode: Option<String>,
+    pub coordinates: Option<Coordinates>,
+    pub timezone: Option<Timezone>,
 }
 #[derive(Debug, Deserialize, Default)]
 struct Name {
@@ -59,22 +63,23 @@ struct Name {
     pub first: String,
     pub last: String,
 }
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 struct User {
-    pub gender: String,
+    pub gender: Option<String>,
     pub name: Name,
-    pub location: Location,
-    pub email: String,
-    pub login: Login,
-    pub dob: DateOfBirth,
-    pub registered: DateOfBirth,
-    pub phone: String,
-    pub cell: String,
-    pub id: Id,
-    pub nat: String,
+    pub location: Option<Location>,
+    pub email: Option<String>,
+    pub login: Option<Login>,
+    pub dob: Option<DateOfBirth>,
+    pub registered: Option<DateOfBirth>,
+    pub phone: Option<String>,
+    pub cell: Option<String>,
+    pub id: Option<Id>,
+    pub nat: Option<String>,
 }
 #[derive(Debug, Deserialize, Default)]
 struct Users {
     pub results: Vec<User>,
+    #[serde(skip_deserializing)]
     info: Info,
 }
